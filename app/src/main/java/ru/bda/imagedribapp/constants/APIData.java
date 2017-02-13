d12fdf81b4ed1c38d07a8f012ef241ba867356fa
belobaba <denbelobaba@gmail.com>
2017-02-13 16:01:10 +0300

    cnahged

---
+++ app/src/main/java/ru/bda/imagedribapp/constants/APIData.java
@@ -0,0 +1,6 @@
+package ru.bda.imagedribapp.constants;
+
+public class APIData {
+    public final static String API_URL = "https://api.dribbble.com/v1/";
+    public final static String CLIENT_ACCESS_TOKEN = "f27661c7354d6a8a3cc44b9c50b744290197f36e4d43bc2c2c436354f1b61124";
+}
